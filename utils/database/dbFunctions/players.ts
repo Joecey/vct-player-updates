bc020828487108573873541cb970c81af03b80ae
Joe Linogao <linogaoj@tcd.ie>
2025-03-11 18:46:39 +0000

    remove old db

--- utils/database/dbFunctions/players.ts
+++ utils/database/dbFunctions/players.ts
@@ -2,15 +2,6 @@ import "jsr:@std/dotenv/load"; // MAKE SURE THIS IS HERE FOR ENV VARIABLES
 import { InsertPlayer, playersSchema } from "../../index.ts";
 import { drizzle } from "drizzle-orm/node-postgres";
 
-// Instantiate Drizzle client with pg driver and schema.
-// export const db = drizzle({
-//   client: new Pool({
-//     connectionString: Deno.env.get("DATABASE_URL")!,
-//   }),
-//   schema: { playersSchema },
-//   prepare: false,
-// });
-
 export const db = drizzle(Deno.env.get("DATABASE_URL")!);
 
 // insert a player into the database
