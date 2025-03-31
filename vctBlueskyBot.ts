9cdf6d72719b0c05472f429638b66725fca064bf
Joe Linogao <linogaoj@tcd.ie>
2025-03-31 21:11:26 +0100

    a

--- vctBlueskyBot.ts
+++ vctBlueskyBot.ts
@@ -20,32 +20,35 @@ import {
 
 console.log(`VCT Player Updates bot started...`);
 
-Deno.cron("Log a message", "*/5 * * * *", async () => {
-  console.log(LogStatus.INFO, `Starting VCT Player Updates bot...`);
-  /* 1) Create map of the the current list of players in the database */
-  const url = Deno.env.get("GOOGLE_SHEET_URL");
-  const response = await fetch(`${url}`);
-  const html = await response.text();
+Deno.cron(
+  "Update VCT Database and post on Bluesky",
+  "*/5 * * * *",
+  async () => {
+    console.log(LogStatus.INFO, `Starting VCT Player Updates bot...`);
+    /* 1) Create map of the the current list of players in the database */
+    const url = Deno.env.get("GOOGLE_SHEET_URL");
+    const response = await fetch(`${url}`);
+    const html = await response.text();
 
-  const playerMetaData = getPlayerRowsFromSheet(html);
-  const googleSheetPlayerTruth = playerMetaData.metadata.playersMap;
+    const playerMetaData = getPlayerRowsFromSheet(html);
+    const googleSheetPlayerTruth = playerMetaData.metadata.playersMap;
 
-  console.log(
-    LogStatus.INFO,
-    `Players found: ${playerMetaData.metadata.staffCount} out of ${playerMetaData.metadata.rowEntriesLength} rows...`,
-  );
+    console.log(
+      LogStatus.INFO,
+      `Players found: ${playerMetaData.metadata.staffCount} out of ${playerMetaData.metadata.rowEntriesLength} rows...`,
+    );
 
-  console.log(
-    LogStatus.INFO,
-    `Players skipped: ${playerMetaData.metadata.skipped} players...`,
-  );
+    console.log(
+      LogStatus.INFO,
+      `Players skipped: ${playerMetaData.metadata.skipped} players...`,
+    );
 
-  /* 2) Get every player ign from database and cross reference with google sheet player map. If a player is not on the
+    /* 2) Get every player ign from database and cross reference with google sheet player map. If a player is not on the
   google sheet, record this and create a new playerRemoved post for bluesky. If a player is on the google sheet, make note
   of this too
   */
 
-  /* 3) For every player identified as still existing, check through each player using the ign and check the following:
+    /* 3) For every player identified as still existing, check through each player using the ign and check the following:
   - NOTE: EACH CHECK NEEDS THE PREVIOUS CHECK TO NOT BE TRUE (prevent multiple posts for the same player)
   - has their team changed? if so, make a new post (playerTeamChanged)
   - has their active status changed? If so, make a new post (playerActiveStatusChanged)
@@ -53,8 +56,9 @@ Deno.cron("Log a message", "*/5 * * * *", async () => {
   - has their contract been extended? if so, make a new post (playerContractUpdated)
   */
 
-  /* 4) in the google sheet map, if a player has not been identified at all, it means that they are a new player.
+    /* 4) in the google sheet map, if a player has not been identified at all, it means that they are a new player.
   Create a new playerAdded post for bluesky and add them to the database as a new entry. */
 
-  /* 5) Gather all posts, and post on bluesky */
-});
+    /* 5) Gather all posts, and post on bluesky */
+  },
+);
